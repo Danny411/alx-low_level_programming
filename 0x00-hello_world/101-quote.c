c0d852d2990bbb24c29e4867f960dbd035e658bb
Danny411 <efevictory142@gmail.com>
2022-09-10 06:23:51 -0700

    C Programming Language

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+include < stdio.h >
+/**
+ * main - pr8nts to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+int main(void)
+{
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long l = 1;
+long syscall = 1;
+long ret = 0;
+_asm_("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
+return (1);
+}
+
