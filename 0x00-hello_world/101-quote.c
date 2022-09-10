749ca7236e9bec108bb56c508a30d3cc99907e34
Danny411 <efevictory142@gmail.com>
2022-09-10 06:34:29 -0700

    C Programming Language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
-long l = 1;
+long L = 1;
 long syscall = 1;
 long ret = 0;
 __asm__("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
