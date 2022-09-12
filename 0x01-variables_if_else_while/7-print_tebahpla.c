408ed834f44dba54959df06da118c8dfb1933dba
Danny411 <efevictory142@gmail.com>
2022-09-11 20:35:54 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - Entry point
+ * Descriotion: print lowercase alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
